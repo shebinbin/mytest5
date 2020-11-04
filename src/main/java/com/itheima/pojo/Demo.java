f3af47ca466d51fe416a9d0ce013134ef0a5f0bd
sbb <1440579920@qq.com>
2020-11-05 00:17:37 +0800

    111

--- src/main/java/com/itheima/pojo/Demo.java
+++ src/main/java/com/itheima/pojo/Demo.java
@@ -3,4 +3,9 @@ package com.itheima.pojo;
 public class Demo {
     private String name;
     private String sex;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
 }
